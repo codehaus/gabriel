cb217638a60ab3b93d4e3e41155ad6a9a3d417d3
stephan <stephan@958a923e-ab72-0410-abe7-cfe17a7a77a3>
2004-07-13 13:43:15 +0000

    Splittet Parser into interface and implementation

--- gabriel/src/java/gabriel/components/parser/AclParser.java
+++ gabriel/src/java/gabriel/components/parser/AclParser.java
@@ -18,15 +18,8 @@
 
 package gabriel.components.parser;
 
-import gabriel.Permission;
 import gabriel.Principal;
 import gabriel.acl.Acl;
-import gabriel.acl.AclEntry;
-
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.StringTokenizer;
 
 /**
  * Parse an ACL from a String with a { } block representation.
@@ -34,7 +27,7 @@ import java.util.StringTokenizer;
  * @author stephan
  * @version $id$
  */
-public class AclParser {
+public interface AclParser {
   /**
    * Parse an Acl description. Needs to throw an exception.
    * <code>
@@ -47,57 +40,5 @@ public class AclParser {
    * @param input Description source
    * @return Parsed Acl
    */
-  public Acl parse(Principal owner, String name, String input) {
-    boolean negative = false;
-
-    Principal principal = null;
-    List permissions = new ArrayList();
-
-    Acl acl = new Acl(owner, name);
-
-    int PERMISSIONS = 1;
-    int PRINCIPAL = 2;
-
-    int state = PRINCIPAL;
-
-    for (StringTokenizer stringTokenizer = new StringTokenizer(input, " \n{}", true);
-         stringTokenizer.hasMoreTokens();) {
-
-      String t = stringTokenizer.nextToken();
-
-      if (" ".equals(t) || "\n".equals(t)) {
-        // do nothing
-      } else if ("{".equals(t)) {
-        state = PERMISSIONS;
-      } else if ("}".equals(t)) {
-        state = PRINCIPAL;
-
-        AclEntry entry = new AclEntry(principal);
-        Iterator iterator = permissions.iterator();
-        while (iterator.hasNext()) {
-          Permission permission = (Permission) iterator.next();
-          entry.addPermission(permission);
-        }
-        if (negative) {
-          entry.setNegativePermissions();
-          negative = false;
-        }
-        acl.addEntry(owner, entry);
-        permissions = new ArrayList();
-        principal = null;
-      } else if (state == PRINCIPAL) {
-        if (t.startsWith("-")) {
-          negative = true;
-          t = t.substring(1);
-        }
-        // handle "- principal" and "-principal"
-        if (!"".equals(t)) {
-          principal = new Principal(t);
-        }
-      } else if (state == PERMISSIONS) {
-        permissions.add(new Permission(t));
-      }
-    }
-    return acl;
-  }
+  public Acl parse(Principal owner, String name, String input);
 }
